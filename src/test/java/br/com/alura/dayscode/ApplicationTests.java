3c57ab04d2d0193b3784d5134e6627fd04f12658
Ugo Cesar de Oliveira Moreira <ugo.moreira@dellavolpe.com.br>
2022-10-18 16:53:18 -0300

    Dia 2 - Alterando o teste para consumir a aplicação local e não o recurso remoto da IMDB.

--- src/test/java/br/com/alura/dayscode/ApplicationTests.java
+++ src/test/java/br/com/alura/dayscode/ApplicationTests.java
@@ -30,7 +30,7 @@ class ApplicationTests {
 	@Test
 	void shouldReturnTop250Films() {
 		String apiKey = "";
-		String imdbResourceUrl = "https://imdb-api.com/en/API/Top250Movies/" + apiKey;
+		String imdbResourceUrl = "http://localhost:" + port + "/api/movies/top?apiKey=" + apiKey;
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("user-agent", "Mozilla/5.0 Firefox/26.0");
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
